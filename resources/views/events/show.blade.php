3e51dca9b68125bb91ae5942ad9daaed12ff1ad3
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-10-07 09:07:12 -0300

    feat: :construction: Criação das Rotas Dinâmicas dos Eventos

---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+@section('title', $event->title)
+
+@section('content')
+
+    <div class="col-md-10 offset-md-1">
+        <div class="row">
+            <div id="image-container" class="col-md-6">
+                <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}" class="img-fluid">
+            </div>
+            <div id="info-container" class="col-md-6">
+                <h1>{{ $event->title }}</h1>
+                <p class="event-city">{{ $event->city }}</p>
+                <p class="events-participants">X Participantes</p>
+                <p class="event-owner">Dono do Evento</p>
+                <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+            </div>
+            <div class="col-md-12" id="description-contiainer">
+                <h3>Sobre o Evento:</h3>
+                <p class="event-description">{{ $event->description  }}</p>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
